151402505c3e3a8ab52f844dab3d0a7bec7d51d8
Irena Grocka <irena.grocka@gmail.com>
2024-02-07 18:48:23 +0000

    add selector event listner and change displatyed city in a dinamic way

--- src/script.js
+++ src/script.js
@@ -1,24 +1,27 @@
 
-function updateTime() {
+function showCityTime(event) {
+    let citiesElement = document.querySelector("#cities");
+    let cityTimezone = event.target.value;
+    let cityName = cityTimezone.replace("_", " ").split("/")[1];
 
-    let londonElement = document.querySelector("#london");
-    let londonDateElement = londonElement.querySelector(".date");
-    let londonTimeElement = londonElement.querySelector(".time");
-    londonTime = moment().tz("Europe/London");
-
-    londonDateElement.innerHTML = londonTime.format("dddd [the] Do [of] MMMM	YYYY");
-    londonTimeElement.innerHTML = `${londonTime.format("h:mm:ss[<small>] A[</small >]")}`;
-
-    //Los Angeles
-    let losAngelesElement = document.querySelector("#los-angeles");
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    losAngelesTime = moment().tz("America/Los_Angeles");
-
-    losAngelesDateElement.innerHTML = losAngelesTime.format("dddd [the] Do [of] MMMM	YYYY");
-    losAngelesTimeElement.innerHTML = `${losAngelesTime.format("h:mm:ss[<small>] A[</small >]")}`;
+    if (cityTimezone.length > 0) {
+        setInterval(() => {
+            let cityTime = moment().tz(cityTimezone);
+            citiesElement.innerHTML = `
+            <div class="city">
+                <span><strong>${cityName}</strong></span>
+                <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
+        `;
+        }, 1000);
+    } else {
+        console.error("Select City", cityTimezone);
+        return;
+    }
 }
 
-updateTime();
+let citySelectElement = document.querySelector("#city-selector");
+citySelectElement.addEventListener("change", showCityTime);
 
-setInterval(updateTime, 1000);
+showCityTime({ target: { value: citySelectElement.value } });
