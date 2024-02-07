c47013f9eb9f11da99826242dc4665e2e5bc77bc
Irena Grocka <irena.grocka@gmail.com>
2024-02-07 19:04:15 +0000

    add my current location timezoen

--- src/script.js
+++ src/script.js
@@ -2,19 +2,34 @@
 function showCityTime(event) {
     let citiesElement = document.querySelector("#cities");
     let cityTimezone = event.target.value;
-    let cityName = cityTimezone.replace("_", " ").split("/")[1];
 
     if (cityTimezone.length > 0) {
-        setInterval(() => {
-            let cityTime = moment().tz(cityTimezone);
-            citiesElement.innerHTML = `
-            <div class="city">
-                <span><strong>${cityName}</strong></span>
-                <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
-            </div>
-            <div class="time">${cityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
-        `;
-        }, 1000);
+        if (cityTimezone === "current") {
+            setInterval(() => {
+                cityTimezone = moment.tz.guess();
+                let yourCityName = cityTimezone.replace("_", " ").split("/")[1];
+                let yourCityTime = moment().tz(cityTimezone);
+                citiesElement.innerHTML = `
+                    <div class="city">
+                        <span><strong>${yourCityName}</strong></span>
+                        <div class="date">${yourCityTime.format("dddd, MMMM Do YYYY")}</div>
+                    </div>
+                    <div class="time">${yourCityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
+                `;
+            }, 1000);
+        } else {
+            setInterval(() => {
+                let cityName = cityTimezone.replace("_", " ").split("/")[1];
+                let cityTime = moment().tz(cityTimezone);
+                citiesElement.innerHTML = `
+                    <div class="city">
+                        <span><strong>${cityName}</strong></span>
+                        <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+                    </div>
+                    <div class="time">${cityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
+                `;
+            }, 1000);
+        }
     } else {
         console.error("Select City", cityTimezone);
         return;
