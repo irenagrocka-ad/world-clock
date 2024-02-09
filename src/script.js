08ab34b420f5e5195b4f3e0c3a341f57e7dd234f
Irena Grocka <irena.grocka@gmail.com>
2024-02-09 18:50:49 +0000

    fixing and improving

--- src/script.js
+++ src/script.js
@@ -1,12 +1,34 @@
-
 let intervalId; // Variable to hold the interval ID
+function updateTime() {
+    // Your
+    let yourCityElement = document.querySelector("#your-city");
+    let yourCityNameElement = document.querySelector(".city-name");
+    let yourCityTimeElement = document.querySelector(".time");
+    let yourCityDateElement = document.querySelector(".date"); // Your Element
+    cityTimezone = moment.tz.guess();
+    let yourCityName = cityTimezone.replace("_", " ").split("/")[1];
+    let yourCityTime = moment().tz(cityTimezone);
+    yourCityNameElement.innerHTML = yourCityName;
+    yourCityTimeElement.innerHTML = yourCityTime.format(
+        "h:mm:ss[<small>] A[</small >]"
+    );
+    yourCityDateElement.innerHTML = yourCityTime.format("dddd, MMMM Do YYYY");
+
+}
+
+updateTime();
+//setInterval(updateTime, 1000); // this function is breaking page when selecting city from dropdown 
+
+
 
 function showCityTime(event) {
+
     let citiesElement = document.querySelector("#cities");
     let cityTimezone = event.target.value;
 
     // Clear previous interval if it exists
     clearInterval(intervalId);
+    console.log("After clearing interval:", intervalId);
 
     if (cityTimezone.length > 0) {
         if (cityTimezone === "current") {
@@ -16,7 +38,12 @@ function showCityTime(event) {
                 let yourCityTime = moment().tz(cityTimezone);
                 citiesElement.innerHTML = `
                     <div class="city">
-                        <span><strong>${yourCityName}</strong></span>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/iikoxwld.json"
+                            trigger="hover"
+                            style="width: 70px; height: 70px"
+                        ></lord-icon>
+                        <span class="city-name"><strong>${yourCityName}</strong></span>
                         <div class="date">${yourCityTime.format("dddd, MMMM Do YYYY")}</div>
                     </div>
                     <div class="time">${yourCityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
@@ -28,7 +55,8 @@ function showCityTime(event) {
                 let cityTime = moment().tz(cityTimezone);
                 citiesElement.innerHTML = `
                     <div class="city">
-                        <span><strong>${cityName}</strong></span>
+                        <img id="city-image" src="src/images/${cityName}.jpg" alt="City image" />
+                        <span class="city-name"><strong>${cityName}</strong></span>
                         <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
                     </div>
                     <div class="time">${cityTime.format("h:mm:ss[<small>] A[</small >]")}</div>
@@ -40,6 +68,7 @@ function showCityTime(event) {
         return;
     }
 }
+
 
 let citySelectElement = document.querySelector("#city-selector");
 citySelectElement.addEventListener("change", showCityTime);
